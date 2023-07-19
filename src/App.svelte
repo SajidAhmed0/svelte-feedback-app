c6585a85fdc6595365021bb43df9f52fb7c17a63
IT21294570 <mjsahmed205@gmail.com>
2023-07-19 10:41:29 +0530

    ready to go

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
+	let feedback = [
+		{
+			id:1,
+			rating: 10,
+			text: 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.'
+		},
+		{
+			id:2,
+			rating: 9,
+			text: 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.'
+		},
+		{
+			id:3,
+			rating: 8,
+			text: 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.'
+		}
+	]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
